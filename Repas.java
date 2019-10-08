16ed311ff0b1c12eb5b6dbcafee5802649d8a29e
Lu <alexis.lu2.sio@gmail.com>
2019-10-08 15:14:12 +0200

    attr Repas

---
+++ Repas.java
@@ -0,0 +1,12 @@
+import java.date.LocalDate;
+class Repas{
+	public int numero;
+	public LocalDate date;
+
+	
+	Repas(int numero, LocalDate date){
+		this.numero = numero;
+		this.date = date;
+	}
+
+}
